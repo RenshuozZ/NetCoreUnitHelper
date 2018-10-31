dd77f5e5883bdddc93e47048c31d2e26ff52e697
rs <285320809@qq.com>
2018-10-31 13:24:43 +0800

    exif

---
+++ Exif/Program.cs
@@ -0,0 +1,23 @@
+﻿using SixLabors.ImageSharp;
+using System;
+using System.IO;
+
+namespace Exif
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (FileStream input = File.OpenRead(@"D:\IMG_0827.JPG"))
+            {
+                var  image =  Image.Load(input);
+                var exif = image.MetaData.ExifProfile.Values;
+                foreach (var item in exif)
+                {
+                    Console.WriteLine(item.Tag + ":" + item.Value);
+                }
+            }
+        }
+     
+    }
+}
