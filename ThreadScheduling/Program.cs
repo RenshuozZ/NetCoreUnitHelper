2d2259f80a108c218ba0c71d536d870cfade3b3f
rs <285320809@qq.com>
2018-11-13 16:38:08 +0800

    ThreadScheduling

---
+++ ThreadScheduling/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadScheduling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 101, 65, 5445, 1, 152, 990 };
+            foreach (var item in numbers)
+            {
+                new Thread(new ThreadStart(delegate () 
+                {
+                    Thread.Sleep(item);
+                    Console.WriteLine(item);
+                })).Start();
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
