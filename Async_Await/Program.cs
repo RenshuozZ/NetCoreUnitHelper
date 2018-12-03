0f62b4facb375930a5e2b80b8e3d00c51b8c167d
rs <285320809@qq.com>
2018-12-03 17:50:25 +0800

    Async_Await

---
+++ Async_Await/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Async_Await
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task<int> t = AddAsync(1, 2);
+
+            //一直在干活
+            Console.WriteLine($"result: {t.Result}");
+            Console.Read();            
+        }
+
+        private static int Add(int n, int m)
+        {
+            return n + m;
+        }
+
+        public static async Task<int> AddAsync(int n, int m)
+        {
+            int val = await Task.Run(() => Add(n, m));
+
+            return val;
+        }
+    }
+}
---
+++ Demo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList obj = new ArrayList();
+            obj.Add(50);
+            obj.Add("Dog");
+            foreach (var i in obj)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
